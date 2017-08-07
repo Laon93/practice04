d13996462afb47867eb92bc592b4e08d0f799357
YeonJin <duswls0515@gmail.com>
2017-08-07 09:10:33 +0900

    17.08.07 09:10

--- src/prob01/Printer.java
+++ src/prob01/Printer.java
@@ -2,7 +2,8 @@ package prob01;
 
 public class Printer {
 	
-	public void println(int i) {
+	//generic 사용하지 않고 일일이 다 정의 해주는 경우
+	/*public void println(int i) {
 		System.out.println(i);
 	}
 	public void println(double d) {
@@ -15,6 +16,13 @@ public class Printer {
 	
 	public void println(String str) {
 		System.out.println(str);
+	}*/
+	
+	public <T> void println(T t) {
+		System.out.println(t);
 	}
+	//이렇게 여러개를 정의할 수도 있음
+	/*public <T,P,Q,R> void println(){
+	}*/
 
 }
